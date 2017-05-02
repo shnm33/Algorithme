0fcefa571ab175c9b5e6b5c085f6e862d29ea9fb
OJG\OJG- <shnm33@naver.com>
2017-05-02 15:11:21 +0900

    [기능구현]minus 완료

--- DS/Math/Math.cpp
+++ DS/Math/Math.cpp
@@ -1,15 +1,19 @@
 #include<stdio.h>
 
 float plus(float a, float b);
+float minus(float a, float b);
 
 int main() {
 	float a=1, b=2.5;
 	char c;
 
-	printf("%f", plus(a, b));
+	printf("%f", minus(a,b));
 
 	return 0;
 }
 float plus(float a, float b) {
 	return a + b;
+}
+float minus(float a, float b) {
+	return a - b;
 }
