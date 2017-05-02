5c8924bd93e041f72bced442c0d7da78db662f21
OJG\OJG- <shnm33@naver.com>
2017-05-02 15:25:00 +0900

    [기능구현] input

--- DS/Math/Math.cpp
+++ DS/Math/Math.cpp
@@ -6,11 +6,12 @@ float muti(float a, float b);
 float divide(float a, float b);
 
 int main() {
-	float a=1, b=2.5;
+	float a, b;
 	char c;
 
-	printf("%f", minus(a,b));
+	scanf("%f %c %f", &a, &c, &b);
 
+	printf("%f %c %f", a, c, b);
 	return 0;
 }
 float plus(float a, float b) {
